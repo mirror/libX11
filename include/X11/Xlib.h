38f9054554d63525d2dd51aafb5eb57821158ab9
Alan Coopersmith <alan.coopersmith@sun.com>
2009-06-15 19:00:43 -0700

    Drop ancient USG SysV #ifdefs
    Signed-off-by: Alan Coopersmith <alan.coopersmith@sun.com>

--- include/X11/Xlib.h
+++ include/X11/Xlib.h
@@ -38,20 +38,7 @@ in this Software without prior written authorization from The Open Group.
 
 #define XlibSpecificationRelease 6
 
-#ifdef USG
-#ifndef __TYPES__
-#include <sys/types.h>			/* forgot to protect it... */
-#define __TYPES__
-#endif /* __TYPES__ */
-#else
-#if defined(_POSIX_SOURCE) && defined(MOTOROLA)
-#undef _POSIX_SOURCE
 #include <sys/types.h>
-#define _POSIX_SOURCE
-#else
-#include <sys/types.h>
-#endif
-#endif /* USG */
 
 #if defined(__SCO__) || defined(__UNIXWARE__)
 #include <stdint.h>
--- src/XlibInt.c
+++ src/XlibInt.c
@@ -3231,7 +3231,7 @@ void _XData32(
  *       and so, you may be better off using gethostname (if it exists).
  */
 
-#if (defined(_POSIX_SOURCE) && !defined(AIXV3) && !defined(__QNX__)) || defined(hpux) || defined(USG) || defined(SVR4)
+#if (defined(_POSIX_SOURCE) && !defined(AIXV3) && !defined(__QNX__)) || defined(hpux) || defined(SVR4)
 #define NEED_UTSNAME
 #include <sys/utsname.h>
 #endif

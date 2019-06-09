5464b302bc0c4a055d3eac06c6ab15e209ff7668
Matt Turner <mattst88@gmail.com>
2019-06-09 14:00:15 -0700

    Use AC_SYS_LARGEFILE
    ... and include config.h in makekeys.c to get the definition of
    _FILE_OFFSET_BITS. Without it, libX11 can fail to build on a file
    system with 64-bit inode numbers.
    
    Bug: https://bugs.gentoo.org/550502
    Bug: https://bugs.gentoo.org/616140
    Signed-off-by: Matt Turner <mattst88@gmail.com>

--- src/util/makekeys.c
+++ src/util/makekeys.c
@@ -28,6 +28,10 @@ from The Open Group.
 
 /* Constructs hash tables for XStringToKeysym and XKeysymToString. */
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>

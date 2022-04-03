mirror/libX11

commit 8a368d808fec166b5fb3dfe6312aab22c7ee20af

Fix two memory leaks in _XFreeX11XCBStructure()
Even when XCloseDisplay() was called, some memory was leaked.

XCloseDisplay() calls _XFreeDisplayStructure(), which calls
_XFreeX11XCBStructure().

However, _XFreeX11XCBStructure() did not destroy the condition variables,
resulting in the leaking of some 40 bytes.

Signed-off-by: Hodong <hodong@yozmos.com>

src/xcb_disp.c
@@ -102,6 +102,8 @@ void _XFreeX11XCBStructure(Display *dpy)
 		dpy->xcb->pending_requests = tmp->next;
 		free(tmp);
 	}
+	xcondition_clear(dpy->xcb->event_notify);
+	xcondition_clear(dpy->xcb->reply_notify);
 	xcondition_free(dpy->xcb->event_notify);
 	xcondition_free(dpy->xcb->reply_notify);
 	Xfree(dpy->xcb);

commit 76d1cc3c1ce943c6ff81dc8c62a1d1b30fabf02e

Don't try to destroy NULL condition variables
This avoids a segfault during error-unwinding if an invalid display name
is passed to XOpenDisplay().

Fixes: 8a368d80 "Fix two memory leaks in _XFreeX11XCBStructure()"
Resolves: #155
Signed-off-by: Simon McVittie <smcv@debian.org>

src/xcb_disp.c
@@ -102,8 +102,10 @@ void _XFreeX11XCBStructure(Display *dpy)
 		dpy->xcb->pending_requests = tmp->next;
 		free(tmp);
 	}
-	xcondition_clear(dpy->xcb->event_notify);
-	xcondition_clear(dpy->xcb->reply_notify);
+	if (dpy->xcb->event_notify)
+		xcondition_clear(dpy->xcb->event_notify);
+	if (dpy->xcb->reply_notify)
+		xcondition_clear(dpy->xcb->reply_notify);
 	xcondition_free(dpy->xcb->event_notify);
 	xcondition_free(dpy->xcb->reply_notify);
 	Xfree(dpy->xcb);